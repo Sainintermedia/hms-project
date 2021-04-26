40fc760224682a7679f4642e97a985f43a8bdd6a
haris0799 <harispurwana07@gmail.com>
2021-04-27 00:30:26 +0700

    haris

--- resources/views/frontend/home/about.blade.php
+++ resources/views/frontend/home/about.blade.php
@@ -3,41 +3,62 @@
 
       <div class="section-title">
         <h2>About</h2>
-        <h3>SEMANGAT BARU DALAM MENCIPTAKAN LAPANGAN PEKERJAAN <span>About Us</span></h3>
-        <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+        <h3>PT. HOKI MAKMUR SEJATI <br> <span>About Us</span></h3>
+        <p>SECURITY SERVICE | LABOUR SUPPLAY | OFFICE SERVICE | IT SOFTWARE HOUSE</p>
       </div>
 
       <div class="row">
         <div class="col-lg-6" data-aos="zoom-out" data-aos-delay="100">
-          <img src="assets/home/img/about.jpg" class="img-fluid" alt="">
+          <img src="assets/home/img/team/hms.jpg" class="img-fluid" alt="">
         </div>
         <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
-          <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+          <h3>PT. Hoki Makmur Sejati</h3>
           <p class="font-italic">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.
+            HOKI MAKMUR SEJATI adalah perusahaan penyedia jasa tenaga kerja,jasa keamanan berlisensi dan
+            profesional. Sebagai perusahaan jasa keamanan yang dapat dipercaya kami mengkhususkan diri 
+            dalam pelayanan petugas keamanan berkualitas tinggi yang mengutamakan kepuasan klien kami.
+            HOKI MAKMUR SEJATI mengembangkan
+            solusi strategi keamanan yang fleksibel
+            untuk memenuhi kebutuhan setiap klien
+            dengan praktik sistem keamanan terbaik.
+            Dikombinasikan dengan perencanaan dan
+            strategi keamanan terbaik, serta
+            kemampuan dan keahlian tim, kami siap
+            memberikan layanan sekuriti yang
+            profesional,
+            Dengan semangat untuk menjadi bagian
+            dari solusi kebutuhan Sumber Daya
+            Manusia yang berkualitas, HOKI
+            MAKMUR SEJATI hadir sebagai mitra
+            kerja yang dapat diandalkan dan kami
+            yakin dapat memberikan pelayanan
+            yang lebih baik serta membantu
+            menyelesaikan segala permasalahan
+            keperluan SDM yang sering dihadapi
+            oleh banyak Perusahaan.
           </p>
           <ul>
             <li>
               <i class="bx bx-store-alt"></i>
               <div>
-                <h5>Ullamco laboris nisi ut aliquip consequat</h5>
-                <p>Magni facilis facilis repellendus cum excepturi quaerat praesentium libre trade</p>
+                <h5>VISI</h5>
+                <p>Menjadi perusahaan Pengelola Jasa
+                  Tenaga Kerja dengan mengutamakan
+                  kepuasan klien dan kesejahteraan
+                  Tenaga Kerja.</p>
               </div>
             </li>
             <li>
               <i class="bx bx-images"></i>
               <div>
-                <h5>Magnam soluta odio exercitationem reprehenderi</h5>
-                <p>Quo totam dolorum at pariatur aut distinctio dolorum laudantium illo direna pasata redi</p>
+                <h5>MISI</h5>
+                <p>Mengembangkan secara inovatif
+                  Sumber Daya Manusia yang 
+                  berkualitas dan professional
+                  dibidangnya.</p>
               </div>
             </li>
           </ul>
-          <p>
-            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </p>
         </div>
       </div>
 
