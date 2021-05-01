c6ce37208a29a15866084b3e576827953006ceed
hiarief <arhistr14@gmail.com>
2021-05-01 13:34:33 +0700

    Arief

--- app/Http/Controllers/GajiController.php
+++ app/Http/Controllers/GajiController.php
@@ -72,7 +72,8 @@ class GajiController extends Controller
             $Slipgaji = DB::table('users as a')
             ->join('karyawan as c','a.nik','=','c.nik')
             ->join('gaji as b','b.nomor_induk','=','c.nomor_induk')
-            ->select('a.id','c.nama','b.nama_jabatan','b.bulan', 'b.tahun', 'c.nomor_induk')
+            ->select('a.id','c.nama','b.nama_jabatan','b.bulan', 'b.tahun', 'c.nomor_induk', 'b.nama_departemen',
+                    'b.gaji_pokok',)
              ->where('a.id','=',Auth::user()->id)
              ->where('b.bulan', '=', $Bulan )->first();
         //    ->first();
--- resources/views/Transaksi/Gaji/slipgaji.blade.php
+++ resources/views/Transaksi/Gaji/slipgaji.blade.php
@@ -7,19 +7,20 @@
         {{ $object->title }}
     @endforeach --}}
 
-@if ($Bulan== ['April', 'Mei', 'Juni', 'Juli'])
+@if ($Bulan== ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember' ])
 
 I have multiple records!
-{{-- @elseif  ($Bulan=="April") --}}
+@elseif  ($Bulan=="Mei")
                 <img class="card-img-top" img src="../assets/img/profile.jpg" alt="Card image cap">
                 <div class="card-body">
                 <h5 class="card-title">	{{ $Slipgaji->nama }}</h5>
-                <p class="card-text">Jabatan : {{ $Slipgaji->nama_jabatan }}</p>
                 <p class="card-text">Nomor Induk : {{ $Slipgaji->nomor_induk }}</p>
-                <p class="card-text">Jumlah : {{ $Slipgaji->bulan }}</p>
+                <p class="card-text">Jabatan : {{ $Slipgaji->nama_jabatan }}</p>
+                <p class="card-text">Depsrtemen : {{ $Slipgaji->nama_departemen }}</p>
+                <p class="card-text">Bulan : {{ $Slipgaji->bulan }}</p>
                 </div>
                 <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Gaji Pokok : {{ $Slipgaji->gaji_pokok }}</li>
                 <li class="list-group-item">Dapibus ac facilisis in</li>
                 <li class="list-group-item">Vestibulum at eros</li>
                 </ul>
@@ -29,7 +30,7 @@ I have multiple records!
                 {{-- @endforeach --}}
             </div>
 @else
-tidak gajian
+Data gaji belum tersedia.
 @endif
 
 
